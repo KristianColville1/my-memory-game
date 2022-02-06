5c90a232e02bb98970c07f2e70368a2a26c732b3
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-06 13:14:54 +0000

    Add card array for images

---
+++ assets/js/index.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentloaded', () => {
+
+    // card options
+    const cardArray = [
+        {
+            name: 'apple',
+            img: './images/apple.jpeg'
+        },
+        {
+            name: 'apple',
+            img: './images/apple.jpeg'
+        },
+        {
+            name: 'banana',
+            img: './images/banana.jpeg'
+        },
+        {
+            name: 'banana',
+            img: './images/banana.jpeg'
+        },
+        {
+            name: 'cherries',
+            img: './images/cherries.jpeg'
+        },
+        {
+            name: 'cherries',
+            img: './images/cherries.jpeg'
+        },
+        {
+            name: 'grape-fruit',
+            img: './images/grape-fruit.jpeg'
+        },
+        {
+            name: 'grape-fruit',
+            img: './images/grape-fruit.jpeg'
+        },
+        {
+            name: 'kiwi',
+            img: './images/kiwi.jpeg'
+        },
+        {
+            name: 'kiwi',
+            img: './images/kiwi.jpeg'
+        }
+    ]
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
