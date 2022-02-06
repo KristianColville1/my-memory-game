0e0c36b1a437df13772c5043e5496d7b21ad23fd
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-06 15:18:58 +0000

    Fix file paths and fix scripts

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,71 +1,78 @@
-document.addEventListener('DOMContentloaded', () => {
-
-    // card options
-    const cardArray = [
-        {
-            name: 'apple',
-            img: 'images/apple.jpeg'
-        },
-        {
-            name: 'apple',
-            img: 'images/apple.jpeg'
-        },
-        {
-            name: 'banana',
-            img: 'images/banana.jpeg'
-        },
-        {
-            name: 'banana',
-            img: 'images/banana.jpeg'
-        },
-        {
-            name: 'cherries',
-            img: 'images/cherries.jpeg'
-        },
-        {
-            name: 'cherries',
-            img: 'images/cherries.jpeg'
-        },
-        {
-            name: 'grape-fruit',
-            img: 'images/grape-fruit.jpeg'
-        },
-        {
-            name: 'grape-fruit',
-            img: 'images/grape-fruit.jpeg'
-        },
-        {
-            name: 'kiwi',
-            img: 'images/kiwi.jpeg'
-        },
-        {
-            name: 'kiwi',
-            img: 'images/kiwi.jpeg'
-        },
-        {
-            name: 'orange',
-            img: 'images/orange.jpeg'
-        },
-        {
-            name: 'orange',
-            img: 'images/orange.jpeg'
-        }
-    ]
-
-    const grid = document.querySelector('.grid');
-
-    // create game board
-    function createBoard(){
-        for(let i = 0; i < cardArray.length; i++){
-            var card = document.createElement('img');
-            card.setAttribute('src', './images/green.jpeg');
-            card.setAttribute('data-id', i);
-            // card.addEventListener('click', flipcard);
-            grid.appendChild(card);                     // Uses the appendChild method
-        }
+const cardArray = [
+    {
+        name: 'apple',
+        img: 'assets/images/apple.jpeg'
+    },
+    {
+        name: 'apple',
+        img: 'assets/images/apple.jpeg'
+    },
+    {
+        name: 'banana',
+        img: 'assets/images/banana.jpeg'
+    },
+    {
+        name: 'banana',
+        img: 'assets/images/banana.jpeg'
+    },
+    {
+        name: 'cherries',
+        img: 'assets/images/cherries.jpeg'
+    },
+    {
+        name: 'cherries',
+        img: 'assets/images/cherries.jpeg'
+    },
+    {
+        name: 'grape-fruit',
+        img: 'assets/images/grape-fruit.jpeg'
+    },
+    {
+        name: 'grape-fruit',
+        img: 'assets/images/grape-fruit.jpeg'
+    },
+    {
+        name: 'kiwi',
+        img: 'assets/images/kiwi.jpeg'
+    },
+    {
+        name: 'kiwi',
+        img: 'assets/images/kiwi.jpeg'
+    },
+    {
+        name: 'orange',
+        img: 'assets/images/orange.jpeg'
+    },
+    {
+        name: 'orange',
+        img: 'assets/images/orange.jpeg'
     }
-    console.log('fuck this bug');
-    createBoard();
+]
+
+const grid = document.querySelector('.grid');
+
+// game board created
+function createBoard() {
+    for(let i = 0; i < cardArray.length; i++){
+        var card = document.createElement('img');
+        card.setAttribute('src', 'assets/images/red-white.jpeg');
+        card.setAttribute('data-id', i);
+        card.addEventListener('click', flipCard());
+        grid.appendChild(card);
+    }
+}
+
+// check for matches
+
+// flip your card
+function flipCard(){
+
+}
 
 
-})
+// update game
+
+
+
+
+createBoard();
