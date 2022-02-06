f6a28016f9c9b8becd64e4fcf46063b0b1042489
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-06 14:47:13 +0000

    Add game board and style

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,58 +4,68 @@ document.addEventListener('DOMContentloaded', () => {
     const cardArray = [
         {
             name: 'apple',
-            img: './images/apple.jpeg'
+            img: 'images/apple.jpeg'
         },
         {
             name: 'apple',
-            img: './images/apple.jpeg'
+            img: 'images/apple.jpeg'
         },
         {
             name: 'banana',
-            img: './images/banana.jpeg'
+            img: 'images/banana.jpeg'
         },
         {
             name: 'banana',
-            img: './images/banana.jpeg'
+            img: 'images/banana.jpeg'
         },
         {
             name: 'cherries',
-            img: './images/cherries.jpeg'
+            img: 'images/cherries.jpeg'
         },
         {
             name: 'cherries',
-            img: './images/cherries.jpeg'
+            img: 'images/cherries.jpeg'
         },
         {
             name: 'grape-fruit',
-            img: './images/grape-fruit.jpeg'
+            img: 'images/grape-fruit.jpeg'
         },
         {
             name: 'grape-fruit',
-            img: './images/grape-fruit.jpeg'
+            img: 'images/grape-fruit.jpeg'
         },
         {
             name: 'kiwi',
-            img: './images/kiwi.jpeg'
+            img: 'images/kiwi.jpeg'
         },
         {
             name: 'kiwi',
-            img: './images/kiwi.jpeg'
+            img: 'images/kiwi.jpeg'
+        },
+        {
+            name: 'orange',
+            img: 'images/orange.jpeg'
+        },
+        {
+            name: 'orange',
+            img: 'images/orange.jpeg'
         }
     ]
 
-    
+    const grid = document.querySelector('.grid');
+
+    // create game board
+    function createBoard(){
+        for(let i = 0; i < cardArray.length; i++){
+            var card = document.createElement('img');
+            card.setAttribute('src', './images/green.jpeg');
+            card.setAttribute('data-id', i);
+            // card.addEventListener('click', flipcard);
+            grid.appendChild(card);                     // Uses the appendChild method
+        }
+    }
+    console.log('fuck this bug');
+    createBoard();
 
 
-
-
-
-
-
-
-
-
-
-
-
-});
+})
